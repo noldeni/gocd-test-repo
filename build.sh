952b1eed590ebec777e3bd97d4bae0a8e6f902f7
Ingo Nolden <dev@nolden.org>
2017-11-02 21:25:00 +0100

    Insert change_release_no.sh into build.sh

--- build.sh
+++ build.sh
@@ -33,5 +33,16 @@ case $key in
   ;;
 esac
 
+SEARCH_VERSION="#UNDEFINED_VERSION#"
+SEARCH_RELEASE="#UNDEFINED_RELEASE#"
+if [ -z ${GO_PIPELINE_COUNTER} ]; then COUNTER="NA"; else COUNTER=$GO_PIPELINE_COUNTER; fi
+LAST_TAG=$(git describe --tag --abbrev=0 2>&1)
+if [ $? -eq 0 ]
+then
+  sed -i "s/${SEARCH_VERSION}/${LAST_TAG}/g" source/conf.py
+  sed -i "s/${SEARCH_RELEASE}/${LAST_TAG}-${COUNTER}/g" source/conf.py
+fi
+
 echo "Building $BUILDTYPE"
 /usr/bin/docker run --rm -i -v "${PWD}:/doc" -u "$(id -u):$(id -g)" eomir/docker-sphinx make $BUILDTYPE
+
